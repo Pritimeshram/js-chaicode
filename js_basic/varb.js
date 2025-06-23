9859b92e22fe952859deab5a1a0b978f3b303180
Priti Meshram <90830967+Pritimeshram@users.noreply.github.com>
2025-06-23 13:26:10 +0000

    var file

---
+++ js_basic/varb.js
@@ -0,0 +1,9 @@
+var accountId=13
+let accountName="priti"
+accountEmal="priti@gmal.com"
+accountId=14
+accountName="loser"
+console.log(accountId);
+console.table([accountId,accountName,accountEmal])
+
+//never use var because of block and functional block
