b3261c0b8b3ae484a8078066691f3a7f0f2e3a81
Priti Meshram <90830967+Pritimeshram@users.noreply.github.com>
2025-07-10 16:55:18 +0000

    con

---
+++ js_basic/conandoper.js
@@ -0,0 +1,45 @@
+/***operstion */
+// console.log(2+2);
+// console.log(2+"2");
+// console.log(2+"2"+2);
+
+// console.log(2+2+"2");
+// console.log("2"+2+"2");
+// console.log("2"+"2"+"2");
+// console.log("2"+"2"+"2"+2+2);
+//  let score = 33
+//  console.log(score);
+//  console.log(typeof score);
+
+//  let score = "33"
+//  console.log(score);
+//  console.log(typeof score);
+ 
+//  let score = "33abs"
+//  console.log(score);
+//  console.log(typeof score);
+//  let numbStr=Number(score)
+//  console.log(numbStr);
+//  console.log(typeof numbStr);
+
+//  let score = undefined
+//  console.log(score);
+//  console.log(typeof score);
+//  let numbStr=Number(score)
+//  console.log(numbStr);
+//  console.log(typeof numbStr);
+
+//  let score = null
+//  console.log(score);
+//  console.log(typeof score);
+//  let numbStr=Number(score)
+//  console.log(numbStr);
+//  console.log(typeof numbStr);
+
+ let score = 33
+ console.log(score);
+ console.log(typeof score);
+ let numbStr=String(score)
+ console.log(numbStr);
+ console.log(typeof numbStr);
+ 
